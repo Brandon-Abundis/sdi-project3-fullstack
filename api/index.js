d15147afe1d315549c344967f5c862765cee28bf
Brandon-Abundis <abrandon99@gmail.com>
2026-03-25 08:56:09 -0600

    initial commit

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => console.log(`Express server listening on port ${port}`));
